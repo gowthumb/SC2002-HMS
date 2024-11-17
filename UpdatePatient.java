62a4882565f56df6ad5a3800da4183c17c0e34eb
DhairyaKakkar <dhairyakakkar23@gmail.com>
2024-11-17 21:43:09 +0800

    first commit

---
+++ UpdatePatient.java
@@ -0,0 +1,5 @@
+import java.io.IOException;
+
+public interface UpdatePatient {
+    public void updatePatients() throws IOException;
+}
---
+++ ViewPatient.java
@@ -0,0 +1,3 @@
+public interface ViewPatient {
+    public void viewPatients();
+}
